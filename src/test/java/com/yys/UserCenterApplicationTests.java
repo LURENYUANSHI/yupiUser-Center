53d3e742ce74c61aeab9e4df564b2a6e365633d4
LURENYUANSHI <lurenyanshi@gmail.com>
2022-09-02 21:19:45 +0800

    1.0

---
+++ src/test/java/com/yys/UserCenterApplicationTests.java
@@ -0,0 +1,20 @@
+package com.yys;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class UserCenterApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
